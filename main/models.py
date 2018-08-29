8066f8357e9d469d546a7ee10ba3290eb205cc20
Cody Weisbach <cody.weisbach@gmail.com>
2018-08-29 05:52:52 -0600

    finalizing index copy, adding mailchimp

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Feedback (models.Model):
+class Feedback(models.Model):
 	text = models.CharField(max_length=1000)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
